768065077b7d803963c6edfd8262769ab3c90777
Daniel Khashabi <danielk@allenai.org>
2020-10-26 02:11:57 -0700

    minor.

--- examples/seq2seq/mytraining.py
+++ examples/seq2seq/mytraining.py
@@ -8,6 +8,8 @@ from torch.utils.data import Dataset, DataLoader, RandomSampler, SequentialSampl
 # Importing the T5 modules from huggingface/transformers
 from mydataset import MyDataset
 from transformers import T5Tokenizer, T5ForConditionalGeneration
+from dataclasses import dataclass, field
+from typing import Dict, Optional
 
 from transformers import (
     AutoConfig,
@@ -41,13 +43,51 @@ IN_LEN = 20
 OUT_LEN = 10
 TRAIN_EPOCHS=20
 
+@dataclass
+class ModelArguments:
+    """
+    Arguments pertaining to which model/config/tokenizer we are going to fine-tune from.
+    """
+
+    model_name_or_path: str = field(
+        metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"}
+    )
+    config_name: Optional[str] = field(
+        default=None, metadata={"help": "Pretrained config name or path if not the same as model_name"}
+    )
+    tokenizer_name: Optional[str] = field(
+        default=None, metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"}
+    )
+    cache_dir: Optional[str] = field(
+        default=None, metadata={"help": "Where do you want to store the pretrained models downloaded from s3"}
+    )
+
+
+@dataclass
+class DataTrainingArguments:
+    """
+    Arguments pertaining to what data we are going to input our model for training and eval.
+    """
+
+    # task_name: str = field(metadata={"help": "The name of the task to train on: " + ", ".join(processors.keys())})
+    data_dir: str = field(metadata={"help": "Should contain the data files for the task."})
+    max_seq_length: int = field(
+        default=128,
+        metadata={
+            "help": "The maximum total input sequence length after tokenization. Sequences longer "
+            "than this will be truncated, sequences shorter will be padded."
+        },
+    )
+    overwrite_cache: bool = field(
+        default=False, metadata={"help": "Overwrite the cached training and evaluation sets"}
+    )
+
 
 
 if __name__ == '__main__':
     # parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
-    parser = HfArgumentParser((TrainingArguments))
-    # model_args, data_args, training_args = parser.parse_args_into_dataclasses()
-    training_args = parser.parse_args_into_dataclasses()
+    parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
+    model_args, data_args, training_args = parser.parse_args_into_dataclasses()
 
     wandb.init(project="finetuning_t5")
 
@@ -99,7 +139,7 @@ if __name__ == '__main__':
     # }
 
     # Set seed
-    set_seed(103)
+    set_seed(training_args.seed)
 
     print(training_args)
 
